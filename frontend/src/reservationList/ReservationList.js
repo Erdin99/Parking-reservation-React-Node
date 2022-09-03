2fa21341ef34c16c2bdc5b4c1f887cd122bf1dd9
Erdin99 <104638072+Erdin99@users.noreply.github.com>
2022-09-03 16:08:48 +0200

    fixed navigation

---
+++ frontend/src/reservationList/ReservationList.js
@@ -0,0 +1,191 @@
+import React, { useEffect, useState } from "react";
+import Navigation from "../navigation/Navigation";
+import "./reservationList.css";
+
+function ReservationList() {
+    
+    const [button1, setButton1] = useState(true);
+    const [button2, setButton2] = useState(false);
+    const [button3, setButton3] = useState(false);
+    const [button4, setButton4] = useState(false);
+
+    function showTodaysReservations() {
+        setButton1(true);
+        setButton2(false);
+        setButton3(false);
+        setButton4(false);
+    }
+
+    function showAllReservations() {
+        setButton1(false);
+        setButton2(true);
+        setButton3(false);
+        setButton4(false);
+    }
+
+    function showApprovedReservations() {
+        setButton1(false);
+        setButton2(false);
+        setButton3(true);
+        setButton4(false);
+    }
+
+    function showDeniedReservations() {
+        setButton1(false);
+        setButton2(false);
+        setButton3(false);
+        setButton4(true);
+    }
+    
+    return(
+        <>
+            <div>
+                <Navigation />
+            </div>
+            <div className="content-admin">
+                {button1 && <h1 className="reservation-list-title">Rezervisana mjesta za: 3.9.2022.</h1>}
+                
+                <div class="button-row">
+                    <button class="button-list-option" onClick={(e) => showTodaysReservations()} style={{backgroundColor: button1 === true?"#26272b":"white", color: button1 === false?"black":"white"}}>Današnje rezervacije</button>
+                    <button class="button-list-option" onClick={(e) => showAllReservations()} style={{backgroundColor: button2 === true?"#26272b":"", color: button2 === true?"white": "black"}}>Dosadašnje rezervacije</button>
+                    <button class="button-list-option" onClick={(e) => showApprovedReservations()} style={{backgroundColor: button3 === true?"#26272b":"", color: button3 === true?"white": "black"}}>Odobrene rezervacije</button>
+                    <button class="button-list-option" onClick={(e) => showDeniedReservations()} style={{backgroundColor: button4 === true?"#26272b":"", color: button4 === true?"white": "black"}}>Odbijene rezervacije</button>
+                </div>
+
+                <div class="list">
+                    <tr>
+                        <th>Username</th>
+                        <th>Parking</th>
+                        <th>Adresa parkinga</th>
+                        <th>Registracijske oznake</th>
+                        <th>Početak rezervacije</th>
+                        <th>Kraj rezervacije</th>
+                        <th>Status rezervacije</th>
+                        <th>{button1 ? "Odgodi rezervaciju" : "Obriši"}</th>
+                    </tr>
+                    {button1 && (
+                        <>
+                            <tr>
+                                <td data-label="Username">Mujke</td>
+                                <td data-label="Parking">Alta parking</td>
+                                <td data-label="Adresa parkinga">Franca Lehara 2</td>
+                                <td data-label="Registracijske oznake">A12-A-123</td>
+                                <td data-label="Početak rezervacije">12:00</td>
+                                <td data-label="Kraj rezervacije">14:00</td>
+                                <td data-label="Status rezervacije">Odobreno</td>
+                                <td data-label="Odgodi rezervaciju"><button className="delete-button">Odgodi</button></td>
+                            </tr>
+                            <tr>
+                                <td data-label="Username">Mujke</td>
+                                <td data-label="Parking">Trg Austrije</td>
+                                <td data-label="Adresa parkinga">Neka ulica 23</td>
+                                <td data-label="Registracijske oznake">A12-A-123</td>
+                                <td data-label="Početak rezervacije">16:00</td>
+                                <td data-label="Kraj rezervacije">18:00</td>
+                                <td data-label="Status rezervacije">Odbijeno</td>
+                                <td data-label="Odgodi rezervaciju"><button className="delete-button">Odgodi</button></td>
+                            </tr>
+                        </>
+                    )}
+
+                    {button2 && (
+                        <>
+                            <tr>
+                                <td data-label="Username">Mujke</td>
+                                <td data-label="Parking">Alta parking</td>
+                                <td data-label="Adresa parkinga">Franca Lehara 2</td>
+                                <td data-label="Registracijske oznake">A12-A-123</td>
+                                <td data-label="Početak rezervacije">12:00</td>
+                                <td data-label="Kraj rezervacije">14:00</td>
+                                <td data-label="Status rezervacije">Odobreno</td>
+                                <td data-label="Obriši"><button className="delete-button">Obriši</button></td>
+                            </tr>
+                            <tr>
+                                <td data-label="Username">Mujke</td>
+                                <td data-label="Parking">Trg Austrije</td>
+                                <td data-label="Adresa parkinga">Neka ulica 23</td>
+                                <td data-label="Registracijske oznake">A12-A-123</td>
+                                <td data-label="Početak rezervacije">16:00</td>
+                                <td data-label="Kraj rezervacije">18:00</td>
+                                <td data-label="Status rezervacije">Odbijeno</td>
+                                <td data-label="Obriši"><button className="delete-button">Obriši</button></td>
+                            </tr>
+                            <tr>
+                                <td data-label="Username">Mujke</td>
+                                <td data-label="Parking">Alta parking</td>
+                                <td data-label="Adresa parkinga">Franca Lehara 2</td>
+                                <td data-label="Registracijske oznake">A12-A-123</td>
+                                <td data-label="Početak rezervacije">12:00</td>
+                                <td data-label="Kraj rezervacije">14:00</td>
+                                <td data-label="Status rezervacije">Odobreno</td>
+                                <td data-label="Obriši"><button className="delete-button">Obriši</button></td>
+                            </tr>
+                            <tr>
+                                <td data-label="Username">Mujke</td>
+                                <td data-label="Parking">Trg Austrije</td>
+                                <td data-label="Adresa parkinga">Neka ulica 23</td>
+                                <td data-label="Registracijske oznake">A12-A-123</td>
+                                <td data-label="Početak rezervacije">16:00</td>
+                                <td data-label="Kraj rezervacije">18:00</td>
+                                <td data-label="Status rezervacije">Odbijeno</td>
+                                <td data-label="Obriši"><button className="delete-button">Obriši</button></td>
+                            </tr>
+                        </>
+                    )}
+                    
+                    {button3 && (
+                        <>
+                            <tr>
+                                <td data-label="Username">Mujke</td>
+                                <td data-label="Parking">Alta parking</td>
+                                <td data-label="Adresa parkinga">Franca Lehara 2</td>
+                                <td data-label="Registracijske oznake">A12-A-123</td>
+                                <td data-label="Početak rezervacije">12:00</td>
+                                <td data-label="Kraj rezervacije">14:00</td>
+                                <td data-label="Status rezervacije">Odobreno</td>
+                                <td data-label="Obriši"><button className="delete-button">Obriši</button></td>
+                            </tr>
+                            <tr>
+                                <td data-label="Username">Mujke</td>
+                                <td data-label="Parking">Trg Austrije</td>
+                                <td data-label="Adresa parkinga">Neka ulica 23</td>
+                                <td data-label="Registracijske oznake">A12-A-123</td>
+                                <td data-label="Početak rezervacije">16:00</td>
+                                <td data-label="Kraj rezervacije">18:00</td>
+                                <td data-label="Status rezervacije">Odobreno</td>
+                                <td data-label="Obriši"><button className="delete-button">Obriši</button></td>
+                            </tr>
+                        </>
+                    )}
+
+                    {button4 && (
+                        <>
+                            <tr>
+                                <td data-label="Username">Mujke</td>
+                                <td data-label="Parking">Alta parking</td>
+                                <td data-label="Adresa parkinga">Franca Lehara 2</td>
+                                <td data-label="Registracijske oznake">A12-A-123</td>
+                                <td data-label="Početak rezervacije">12:00</td>
+                                <td data-label="Kraj rezervacije">14:00</td>
+                                <td data-label="Status rezervacije">Odbijeno</td>
+                                <td data-label="Obriši"><button className="delete-button">Obriši</button></td>
+                            </tr>
+                            <tr>
+                                <td data-label="Username">Mujke</td>
+                                <td data-label="Parking">Trg Austrije</td>
+                                <td data-label="Adresa parkinga">Neka ulica 23</td>
+                                <td data-label="Registracijske oznake">A12-A-123</td>
+                                <td data-label="Početak rezervacije">16:00</td>
+                                <td data-label="Kraj rezervacije">18:00</td>
+                                <td data-label="Status rezervacije">Odbijeno</td>
+                                <td data-label="Obriši"><button className="delete-button">Obriši</button></td>
+                            </tr>
+                        </>
+                    )}
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default ReservationList;
